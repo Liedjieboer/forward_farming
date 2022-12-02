1538bfa6a313b31fa5dd369bb7531be0354600ab
Nico Marais <nico.marais@gmail.com>
2022-12-02 09:15:58 +0000

    Added background image for streamlit app

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -1,23 +1,33 @@
 import streamlit as st
+import base64
 
 st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
+    page_title="Forward Farming",
+    page_icon=":tractor:",
 )
 
-st.markdown("<h1 style='font-size:80px; font-family:courier new; text-align: center; color: white;'>Forward Farming</h1>", unsafe_allow_html=True)
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('boudewijn-boer-qT515JdZNy8-unsplash.jpg')
 
-# st.write("# :tractor: Welcome to Forward Farming! :tractor:")
-
-from PIL import Image
-image = Image.open('/Users/nico_marais/code/Liedjieboer/forward_farming_big_datasets/pics/boudewijn-boer-qT515JdZNy8-unsplash.jpg')
-
-st.image(image)
+st.markdown("<h1 style='font-size:70px; font-family:courier new; text-align: center; color: black;'>Forward Farming</h1>", unsafe_allow_html=True)
 
 st.markdown(
-"""<h1 style='font-size:30px;
+"""<h1 style='font-size:25px;
 font-family:courier new;
 text-align: center;
-color: white;
+color: black;
 '>When will Sheffield have its day in the sun?</h1>"""
 , unsafe_allow_html=True)
