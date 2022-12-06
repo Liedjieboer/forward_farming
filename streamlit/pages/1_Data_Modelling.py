a3d61fe48e3d21da0f69ddaf0df044962c4c0bdf
Nico Marais <nico.marais@gmail.com>
2022-12-06 17:33:08 +0000

    fix one - url nonsense

--- streamlit/pages/1_Data_Modelling.py
+++ streamlit/pages/1_Data_Modelling.py
@@ -7,21 +7,17 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('vineyard.jpg')
+st.markdown(
+f"""
+<style>
+.stApp {{
+    background-image: url(https://raw.githubusercontent.com/Liedjieboer/forward_farming/master/streamlit/vineyard.jpg);
+    background-size: cover
+}}
+</style>
+""",
+unsafe_allow_html=True
+)
 
 st.markdown("<h1 style='font-size:70px; font-family:didot; text-align: center; color: black;'>What we did</h1>", unsafe_allow_html=True)
 
