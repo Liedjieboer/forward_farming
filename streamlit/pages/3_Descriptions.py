8429475578eab05a1c0f2de8379c00160417b290
Nico Marais <nico.marais@gmail.com>
2022-12-08 11:45:15 +0000

    Last-minute changes 1

--- streamlit/pages/3_Descriptions.py
+++ streamlit/pages/3_Descriptions.py
@@ -1,5 +1,5 @@
 import streamlit as st
-# import base64
+import time
 from wordcloud_gen.wordcloud_year import WordcloudYear
 
 st.set_page_config(
@@ -21,8 +21,40 @@ unsafe_allow_html=True
 )
 
 st.markdown("<h1 style='font-size:70px; font-family:didot; text-align: center; color: black;'>What will our wine taste like?</h1>", unsafe_allow_html=True)
+WordcloudYear.wordcloud_gen('bad')
+WordcloudYear.wordcloud_gen('good')
+WordcloudYear.wordcloud_gen('excellent')
 
 option = st.select_slider('', [2072, 2122, 2172])
 option_dict = {2072:'bad', 2122:'good', 2172:'excellent'}
 if option is not None:
-    st.write(WordcloudYear.wordcloud_gen(option_dict[option]))
+    if option == 2072:
+        bar = st.progress(0)
+
+        for i in range(100):
+            # Update the progress bar with each iteration.
+            # latest_iteration.text(f'Iteration {i+1}')
+            bar.progress(i + 1)
+            time.sleep(0.1)
+
+        st.image("temp_bad.png")
+    elif option == 2122:
+        bar = st.progress(0)
+
+        for i in range(100):
+            # Update the progress bar with each iteration.
+            # latest_iteration.text(f'Iteration {i+1}')
+            bar.progress(i + 1)
+            time.sleep(0.1)
+
+        st.image("temp_good.png")
+    elif option == 2172:
+        bar = st.progress(0)
+
+        for i in range(100):
+            # Update the progress bar with each iteration.
+            # latest_iteration.text(f'Iteration {i+1}')
+            bar.progress(i + 1)
+            time.sleep(0.1)
+
+        st.image("temp_excellent.png")
