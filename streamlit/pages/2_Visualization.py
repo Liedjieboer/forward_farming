f0192ac3191314c14d3997d71bc775d2a5bc869a
Nico Marais <nico.marais@gmail.com>
2023-01-26 16:19:17 +0000

    Image size changes 1

--- streamlit/pages/2_Visualization.py
+++ streamlit/pages/2_Visualization.py
@@ -10,17 +10,17 @@ option = st.selectbox(
 
 if option == 'Sites':
     st.write(
-        f"<iframe src='https://cesium.com/ion/stories/viewer/?id=94b064a0-668f-495d-bf86-bdca2364f818' width='700' height='400'></iframe>",
-        unsafe_allow_html=True,
+        f"<iframe src='https://cesium.com/ion/stories/viewer/?id=94b064a0-668f-495d-bf86-bdca2364f818' width='1000' height='700'></iframe>",
+        unsafe_allow_html=True
     )
 
 elif option == 'Temperature':
     # col1, col2, col3 = st.columns(3)
     # with col1:
 
-    col1, col2 = st.columns([14, 1])
+    col1, col2 = st.columns([7, 1])
     col1.write(
-        f"<iframe src='https://cesium.com/ion/stories/viewer/?id=5fdeb51a-4be7-4231-9f4f-e0c9d77aed12' width='600' height='400'></iframe>", #width='500' height='400'
-        unsafe_allow_html=True,
+        f"<iframe src='https://cesium.com/ion/stories/viewer/?id=5fdeb51a-4be7-4231-9f4f-e0c9d77aed12' width='900' height='700'></iframe>", #width='500' height='400'
+        unsafe_allow_html=True
     )
     col2.image(new_image)
